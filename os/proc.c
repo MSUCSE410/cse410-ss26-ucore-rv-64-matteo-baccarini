37c88e2bc60782054358e9789dbcfcc33cad1a1e
Matteo Baccarini <baccarinimatteo5@gmail.com>
2026-02-19 18:27:10 +0000

    proc

--- os/proc.c
+++ os/proc.c
@@ -29,10 +29,13 @@ void proc_init(void)
 	for (p = pool; p < &pool[NPROC]; p++) {
 		p->state = UNUSED;
 		p->kstack = (uint64)kstack[p - pool];
+		p->ustack = (uint64)ustack[p - pool];
 		p->trapframe = (struct trapframe *)trapframe[p - pool];
 		/*
 		* LAB1: you may need to initialize your new fields of proc here
 		*/
+		p->time = 0;
+		memset(p->syscall_times, 0, sizeof(p->syscall_times));
 	}
 	idle.kstack = (uint64)boot_stack_top;
 	idle.pid = 0;
